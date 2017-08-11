8ade8d2f33c8595e3aefead00d14547ea00f6614
Ekaterina Tokareva <Tokaree@labcorp.com>
2017-08-11 19:05:59 +0300

    classes rename

--- src/index.js
+++ src/index.js
@@ -4,11 +4,9 @@ import {SpyItemDirective} from './scrollSpy/spyItemDirective';
 import {SpyListItemDirective} from './scrollSpy/spyListItemDirective';
 import {SpyGroupDirective} from './scrollSpy/spyGroupDirective';
 
-export default function() {
-  angular.module('scroll-spy', [])
-	.directive('scrollSpy', ScrollSpyDirective.createInstance)
-	.directive('scrollSpyArea', ScrollSpyAreaDirective.createInstance)
-	.directive('spyItem', SpyItemDirective.createInstance)
-	.directive('spyListItem', SpyListItemDirective.createInstance)
-	.directive('spyGroup', SpyGroupDirective.createInstance);
-}
+angular.module('scroll-spy', [])
+  .directive('scrollSpy', ScrollSpyDirective.createInstance)
+  .directive('scrollSpyArea', ScrollSpyAreaDirective.createInstance)
+  .directive('spyItem', SpyItemDirective.createInstance)
+  .directive('spyListItem', SpyListItemDirective.createInstance)
+  .directive('spyGroup', SpyGroupDirective.createInstance);
